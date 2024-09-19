2cc91bd036dfb4682b2567364bd646a18a3ee9ff
HamBeomJoon <hbj0802@naver.com>
2024-09-19 14:47:37 +0900

    B2_2154_수이어쓰기3

---
+++ solvedac/random_marathon/week16/2154_수이어쓰기3.kt
@@ -0,0 +1,1 @@
+package week16import java.io.BufferedReaderimport java.io.InputStreamReaderfun main() {    val br = BufferedReader(InputStreamReader(System.`in`))    val sb = StringBuilder()    val n = br.readLine().toInt()    for (i in 1..n) {        sb.append(i)    }    println(sb.indexOf(n.toString()) + 1)}
