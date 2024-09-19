8d5335ad4b85c98b3041968610bd00cea17b37c1
HamBeomJoon <hbj0802@naver.com>
2024-09-19 14:47:23 +0900

    B5_14581_팬들에게둘러싸인홍준

---
+++ solvedac/random_marathon/week16/14581_팬들에게둘러싸인홍준.kt
@@ -0,0 +1,1 @@
+package week16import java.io.BufferedReaderimport java.io.InputStreamReaderfun main() {    val br = BufferedReader(InputStreamReader(System.`in`))    val s = br.readLine()    println(":fan::fan::fan:")    println(":fan::$s::fan:")    println(":fan::fan::fan:")}
