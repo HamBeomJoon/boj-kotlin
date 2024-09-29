e6aea4bbb1f02eef394033af57b5cfeb50c50521
HamBeomJoon <hbj0802@naver.com>
2024-09-29 15:42:03 +0900

    style: 코드라인 CR -> LF로 변경

--- solvedac/random_marathon/week16/28239_배고파.kt
+++ solvedac/random_marathon/week16/28239_배고파.kt
@@ -1,1 +1,25 @@
-package week16import java.io.BufferedReaderimport java.io.InputStreamReaderfun main() {    val br = BufferedReader(InputStreamReader(System.`in`))    val n = br.readLine().toInt()    repeat(n) {        val m = br.readLine().toLong()        menu(m)    }}fun menu(m: Long) {    for (i in 0..60) {        for (j in i..60) {            if ((1L shl i) + (1L shl j) == m) {                println("$i $j")                return            }        }    }}
+package week16
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    repeat(n) {
+        val m = br.readLine().toLong()
+        menu(m)
+    }
+}
+
+fun menu(m: Long) {
+    for (i in 0..60) {
+        for (j in i..60) {
+            if ((1L shl i) + (1L shl j) == m) {
+                println("$i $j")
+                return
+            }
+        }
+    }
+}
--- solvedac/random_marathon/week16/31925_APC2shake.kt
+++ solvedac/random_marathon/week16/31925_APC2shake.kt
@@ -1,1 +1,29 @@
-package week16import java.io.BufferedReaderimport java.io.InputStreamReaderimport java.util.StringTokenizerimport kotlin.math.ceilimport kotlin.math.maximport kotlin.math.minfun main() {    val br = BufferedReader(InputStreamReader(System.`in`))    val n = br.readLine().toInt()    var candidate: MutableList<List<String>> = mutableListOf()    repeat(n) {        val input = br.readLine().split(" ")        if (input[1] == "jaehak" && input[2] != "winner" && (input[3].toInt() > 3 || input[3].toInt() == -1)) {            candidate.add(listOf(input[0], input[4]))        }    }    var sortedCandidate = candidate.sortedBy { it[1].toInt() }    if (sortedCandidate.size > 10) {        sortedCandidate = sortedCandidate.take(10)    }    val ans = sortedCandidate.sortedBy { it[0] }    println(ans.size)    ans.forEach { println(it[0]) }}
+package week16
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.math.ceil
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    var candidate: MutableList<List<String>> = mutableListOf()
+
+    repeat(n) {
+        val input = br.readLine().split(" ")
+        if (input[1] == "jaehak" && input[2] != "winner" && (input[3].toInt() > 3 || input[3].toInt() == -1)) {
+            candidate.add(listOf(input[0], input[4]))
+        }
+    }
+
+    var sortedCandidate = candidate.sortedBy { it[1].toInt() }
+    if (sortedCandidate.size > 10) {
+        sortedCandidate = sortedCandidate.take(10)
+    }
+    val ans = sortedCandidate.sortedBy { it[0] }
+    println(ans.size)
+    ans.forEach { println(it[0]) }
+}
