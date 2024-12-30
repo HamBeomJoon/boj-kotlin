47e43ac56e7a9c296f3fc98eda68a49a3c5e3a20
HamBeomJoon <hbj0802@naver.com>
2024-12-30 13:58:14 +0900

    랜덤마라톤 week30 B번

---
+++ solvedac/random_marathon/week30/11880_개미.kt
@@ -0,0 +1,17 @@
+package week30
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    repeat(br.readLine().toInt()) {
+        val (a, b, c) = br.readLine().split(" ").map { it.toInt() }
+        val caseA = (pow2(a) + pow2(b + c))
+        val caseB = (pow2(b) + pow2(c + a))
+        val caseC = (pow2(c) + pow2(a + b))
+        println(minOf(caseA, caseB, caseC))
+    }
+}
+
+fun pow2(x: Int): Int = x * x
