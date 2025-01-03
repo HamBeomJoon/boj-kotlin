f65e341cb8f00b8eb2729c6cf29b890065fe003d
HamBeomJoon <hbj0802@naver.com>
2025-01-03 11:30:17 +0900

    solved Class 1 17362번

---
+++ boj/src/bronze/31403_A+B-C.kt
@@ -0,0 +1,13 @@
+package bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val a = br.readLine()
+    val b = br.readLine()
+    val c = br.readLine()
+    println(a.toInt() + b.toInt() - c.toInt())
+    println((a + b).toInt() - c.toInt())
+}
